86886f79d71fb093a486719ede48d5015a1fed49
Anthony J <anthony.jones@no-reply.com>
2020-02-03 13:19:01 -0500

    dictionary lab

---
+++ dictrev03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def main():
+
+    vendordict = {'cisco': True, 'juniper': False, 'arista': True, 'netgear': True}
+    custlist = ['acme', 'gloabex corpaoration', 'soylent green', 'initech', 'umbrella corporation']
+
